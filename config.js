c23bf2f36c862c59dbc84724e78fea3bfa77f2dc
Matheus Blödorn <blodorn.matheus@gmail.com>
2017-04-01 18:31:01 -0300

    hello heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const MONGO_USER = 'test';
 const MONGO_PASS = 'test';
-const MONGO_SERVER = 'ds119728.mlab.com:19728/heroku_gxnl2pkx';
+const MONGO_SERVER = 'ds149030.mlab.com:49030/heroku_00vlg1jl';
 
 const MONGO_URL = `mongodb://${MONGO_USER && MONGO_PASS ? `${MONGO_USER}:${MONGO_PASS}@` : ''}${MONGO_SERVER}`;
 const SERVER_PORT = 3000;
